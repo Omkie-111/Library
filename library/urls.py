7c20c6e1b857331490922e33ff60c1c472433ad7
Omkie-111 <omkiepachauli@gmail.com>
2024-12-20 10:31:50 +0530

    Added /borrow/ endpoint for making POST/PUT operations over borrow

--- library/urls.py
+++ library/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AuthorViewSet, BookViewSet
+from .views import AuthorViewSet, BookViewSet, BorrowRecordViewSet
 
 router = DefaultRouter()
 router.register('authors', AuthorViewSet, basename='author')
 router.register('books', BookViewSet, basename='book')
+router.register('borrow', BorrowRecordViewSet, basename='borrow')
 
 urlpatterns = [
     path('', include(router.urls)),
--- library/views.py
+++ library/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets
-from .models import Author, Book
-from .serializers import AuthorSerializer, BookSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Author, Book, BorrowRecord
+from .serializers import AuthorSerializer, BookSerializer, BorrowRecordSerializer
+from datetime import datetime
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
@@ -10,3 +13,20 @@ class AuthorViewSet(viewsets.ModelViewSet):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BorrowRecordViewSet(viewsets.ModelViewSet):
+    queryset = BorrowRecord.objects.all()
+    serializer_class = BorrowRecordSerializer
+
+    @action(detail=True, methods=['put'], url_path='return')
+    def return_book(self, request, pk=None):
+        borrow_record = self.get_object()
+        if borrow_record.return_date is not None:
+            return Response({"detail": "Book already returned."}, status=status.HTTP_400_BAD_REQUEST)
+
+        borrow_record.return_date = datetime.now().date()
+        borrow_record.book.available_copies += 1
+        borrow_record.book.save()
+        borrow_record.save()
+        return Response({"detail": "Book returned successfully."}, status=status.HTTP_200_OK)
